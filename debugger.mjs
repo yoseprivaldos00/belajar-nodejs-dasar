e200c4ec02f615217c2b42c657d525ebc21351bf
Yosep Rivaldo Silaban <rivaldo.yoseps@gmail.com>
2022-08-06 09:03:56 +0700

    modul debugger di nodejs

---
+++ debugger.mjs
@@ -0,0 +1,11 @@
+function sayHello(name) {
+   debugger;
+   return `Hello ${name}`
+}
+
+const firstName = "Yosep";
+
+console.info(sayHello(firstName));
+
+// untuk menjalankan mode debug
+// node inspect debugger.mjs
